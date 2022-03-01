687605331a5f14285a701e6687e381495a890c8f
Joseph DeChicchis <jdechicchis@clearstreet.io>
2022-03-01 03:09:41 +0000

    Use some grpc

--- kafka-ui-api/src/main/java/com/provectus/kafka/ui/client/BufSchemaRegistryClient.java
+++ kafka-ui-api/src/main/java/com/provectus/kafka/ui/client/BufSchemaRegistryClient.java
@@ -1,7 +1,21 @@
 package com.provectus.kafka.ui.client;
 
+import com.provectus.kafka.ui.proto.gen.buf.alpha.registry.v1alpha1.ImageServiceGrpc;
+import io.grpc.Channel;
+import io.grpc.ManagedChannelBuilder;
+
 public class BufSchemaRegistryClient {
-  public BufSchemaRegistryClient() {}
+  public BufSchemaRegistryClient() {
+  }
+
+  public BufSchemaRegistryClient(String host, int port) {
+    this(ManagedChannelBuilder.forAddress(host, port).usePlaintext());
+  }
+
+  public BufSchemaRegistryClient(ManagedChannelBuilder<?> channelBuilder) {
+    Channel channel = channelBuilder.build();
+    Object blockingStub = ImageServiceGrpc.newBlockingStub(channel);
+  }
 
   // TODO make this do something
 }
